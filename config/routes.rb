6bbebde002b73bf64b4c1c5a3ba27d809c6e1239
Dhruv Chandarana <chandaranadhruv24@gmail.com>
2017-01-25 20:21:06 +0000

    Add a static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
    resources :microposts
   resources :users
   root 'users#index'
+    get  'static_pages/home'
+  get  'static_pages/help'
 end
